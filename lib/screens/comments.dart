91c282e6062a29f6caa7744c69bf875b651323f4
Kathirvel Chandrasekaran <kathirvel98@outlook.com>
2021-07-22 18:55:31 +0530

    UI changes in comments

--- lib/screens/comments.dart
+++ lib/screens/comments.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:comment_box/comment/comment.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:food_for_all/services/createPostService.dart';
+import 'package:food_for_all/utils/theming.dart';
 
 class Comments extends StatefulWidget {
   QueryDocumentSnapshot snapshot;
@@ -25,68 +27,51 @@ class _CommentsState extends State<Comments> {
 
   final formKey = GlobalKey<FormState>();
   final TextEditingController commentController = TextEditingController();
-  List filedata = [
-    {
-      'name': 'Adeleye Ayodeji',
-      'pic': 'https://picsum.photos/300/30',
-      'message': 'I love to code'
-    },
-    {
-      'name': 'Biggi Man',
-      'pic': 'https://picsum.photos/300/30',
-      'message': 'Very cool'
-    },
-    {
-      'name': 'Biggi Man',
-      'pic': 'https://picsum.photos/300/30',
-      'message': 'Very cool'
-    },
-    {
-      'name': 'Biggi Man',
-      'pic': 'https://picsum.photos/300/30',
-      'message': 'Very cool'
-    },
-  ];
 
-  Widget commentChild(data) {
-    return ListView(
-      children: [
-        for (var i = 0; i < widget.snapshot['comments'].length; i++)
-          Padding(
-            padding: const EdgeInsets.fromLTRB(2.0, 8.0, 2.0, 0.0),
-            child: ListTile(
-              leading: Container(
-                height: 50.0,
-                width: 50.0,
-                decoration: new BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: new BorderRadius.all(
-                    Radius.circular(50),
+  Widget commentChild() {
+    return Consumer(
+      builder: (context, watch, child) {
+        final theme = watch(themingNotifer);
+        return ListView(
+          children: [
+            for (var i = 0; i < widget.snapshot['comments'].length; i++)
+              Padding(
+                padding: const EdgeInsets.fromLTRB(2.0, 8.0, 2.0, 0.0),
+                child: ListTile(
+                  leading: Container(
+                    height: 50.0,
+                    width: 50.0,
+                    decoration: new BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: new BorderRadius.all(
+                        Radius.circular(50),
+                      ),
+                    ),
+                    child: CircleAvatar(
+                      radius: 50,
+                      backgroundImage: NetworkImage(
+                        widget.snapshot['comments'][i]['commentedPhotoURL'],
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    widget.snapshot['comments'][i]['commentedBy'],
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: !theme.darkTheme ? Colors.white : Colors.black,
+                    ),
+                  ),
+                  subtitle: Text(
+                    widget.snapshot['comments'][i]['comment'],
+                    style: TextStyle(
+                      color: !theme.darkTheme ? Colors.white60 : Colors.black45,
+                    ),
                   ),
                 ),
-                child: CircleAvatar(
-                  radius: 50,
-                  backgroundImage: NetworkImage(
-                    widget.snapshot['comments'][i]['commentedPhotoURL'],
-                  ),
-                ),
-              ),
-              title: Text(
-                widget.snapshot['comments'][i]['commentedBy'],
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              subtitle: Text(
-                widget.snapshot['comments'][i]['comment'],
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          )
-      ],
+              )
+          ],
+        );
+      },
     );
   }
 
@@ -107,7 +92,7 @@ class _CommentsState extends State<Comments> {
         child: Container(
           child: CommentBox(
             userImage: widget.snapshot['photo'],
-            child: commentChild(filedata),
+            child: commentChild(),
             labelText: 'Write a comment...',
             withBorder: false,
             errorText: 'Comment cannot be blank',
@@ -129,7 +114,7 @@ class _CommentsState extends State<Comments> {
             sendWidget: Icon(
               Icons.send_rounded,
               size: 30,
-              color: Colors.white,
+              color: Theme.of(context).accentColor,
             ),
           ),
         ),
