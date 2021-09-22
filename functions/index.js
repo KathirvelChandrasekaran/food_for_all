bf0414144763b7c8258e924bee7cafa3f2fef334
Kathirvel Chandrasekaran <kathirvel98@outlook.com>
2021-09-22 14:18:26 +0530

    added: firebase function for algolia search

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,29 @@
 const functions = require("firebase-functions");
+const algoliasearch = require("algoliasearch");
+const APP_ID = functions.config().algolia.app;
+const ADMIN_KEY = functions.config().algolia.key;
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const client = algoliasearch(APP_ID, ADMIN_KEY);
+const index = client.initIndex("posts");
+
+exports.addToIndex = functions.firestore
+  .document("Posts/{id}")
+  .onCreate((snapshot) => {
+    const data = snapshot.data();
+    const objectID = snapshot.id;
+    return index.addObject({ ...data, objectID });
+  });
+
+exports.updateIndex = functions.firestore
+  .document("Posts/{id}")
+  .onUpdate((changes) => {
+    const newData = changes.after.data();
+    const objectID = changes.after.id;
+    return index.saveObject({ ...newData, objectID });
+  });
+
+exports.deleteFromIndex = functions.firestore
+  .document("Posts/{id}")
+  .onDelete((snapshot) => {
+    index.deleteObject(snapshot.id);
+  });
